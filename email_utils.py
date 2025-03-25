9390cb5704965cab8c8e2eac7c9e8263f8a3062a
Қонысбек Қуаныш <00057394@nb>
2025-03-25 18:46:11 +0500

    feat: connect env variables

--- email_utils.py
+++ email_utils.py
@@ -1,5 +1,10 @@
 import smtplib
 from email.mime.text import MIMEText
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Загружаем переменные из .env
+
 
 def send_booking_email(booking):
     msg = MIMEText(f"""
@@ -21,10 +26,15 @@ Email: {booking.email}
 
 Итого: {booking.total_price} AED
 """)
+
+    from_email = os.getenv("EMAIL_USER")
+    to_email = os.getenv("EMAIL_TO")
+    password = os.getenv("EMAIL_PASS")
+
     msg['Subject'] = f"Бронирование: {booking.excursion_title} ({booking.date})"
-    msg['From'] = "your@email.com"
-    msg['To'] = "operator@email.com"
+    msg['From'] = from_email
+    msg['To'] = to_email
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
-        server.login("your@email.com", "your_app_password")
+        server.login(from_email, password)
         server.send_message(msg)
